a1329668fb7f572b28bd30a146c9a837507ead13
Mendel Bak <mendelbak@gmail.com>
2018-06-15 10:45:05 -0500

    Initial commit. Nearly finished. Filter function works well, AJAX queries working and displaying user data in tables correctly. In middle of pagination feature which is working 90%. Need to get correct number of page numbers to display.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Data;
+using AjaxPagination.Models;
+
+namespace AjaxPagination.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private UserContext _context;
+
+        public HomeController(UserContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            // This variable determines how many results to display per "page" and subsequently how many "pages" the view needs to display them all.
+            int UsersPerPage = 3;
+            List<User> AllUsers = _context.Users.Take(UsersPerPage).ToList();
+            ViewBag.NumOfPages = (_context.Users.Count() / 3);
+            ViewBag.AllUsers = AllUsers;
+            return View();
+        }
+
+
+        // I can build two additional overloaded methods here, that take in 1) LastName, 2) LastName, StartDate, 3) LastName, StartDate, EndDate.
+        // Instead of overloaded methods, I'm going to make this single FilterUsers method have default Start and End dates.
+        [HttpPost]
+        [Route("FilterUsers")]
+        public JsonResult FilterUsers(string LastName, DateTime? StartDate = null, DateTime? EndDate = null, int selectedPage = 1)
+        {
+            List<User> AllUsers;
+            int UsersPerPage = 3;
+
+            // There are waaay too many if/else checks here. There has to be a better way to do this. This feels like an immensely poor implementation.
+            if (LastName == "GetAllUsers" && StartDate == null && EndDate == null)
+            {
+                AllUsers = _context.Users.Skip(selectedPage * UsersPerPage - 3).Take(UsersPerPage).ToList();
+            }
+            else if (LastName == "GetAllUsers" && StartDate != null && EndDate == null)
+            {
+                AllUsers = _context.Users.Where(user => (user.created_at > (DateTime)StartDate)).ToList();
+            }
+            else if (LastName == "GetAllUsers" && EndDate != null && StartDate == null)
+            {
+                AllUsers = _context.Users.Where(user => (user.created_at < (DateTime)EndDate)).ToList();
+            }
+            else if (LastName == "GetAllUsers" && EndDate != null && StartDate != null)
+            {
+                AllUsers = _context.Users.Where(user => (user.created_at > (DateTime)StartDate) && (user.created_at < (DateTime)EndDate)).ToList();
+            }
+            else if (StartDate == null && EndDate == null)
+            {
+                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName)).ToList();
+            }
+            else if (EndDate == null)
+            {
+                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName) && (user.created_at > (DateTime)StartDate)).ToList();
+            }
+            else if (StartDate == null)
+            {
+                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName) && (user.created_at < (DateTime)EndDate)).ToList();
+            }
+            else
+            {
+                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName) && (user.created_at > (DateTime)StartDate) && (user.created_at < (DateTime)EndDate)).ToList();
+            }
+            ViewBag.UsersPerPage = UsersPerPage;
+            ViewBag.AllUsers = AllUsers;
+            return Json(AllUsers);
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,47 @@
+﻿// Write your JavaScript code.
+$(document).ready(function () {
+
+    // Ensures that no null values are passed (they create a compiler error).
+    function checkInputField(selectedPage) {
+        let noNullLastName = "";
+        let startDate = $("#startDate").val();
+        let endDate = $("#endDate").val();
+        
+        if ($("#LastName").val().trim() === "") {
+            // If no string is entered by the user return a string of "GetAllUsers".
+            noNullLastName = "GetAllUsers";
+        } else {
+            noNullLastName = $("#LastName").val().trim();
+        }
+        FilterUsers(noNullLastName, startDate, endDate, selectedPage);
+    }
+
+    // Performs the AJAX POST call to the controller.
+    function FilterUsers(LastName, startDate, endDate, selectedPage) {
+        // POST function call with LastName as an arguement.
+        $.post("FilterUsers/", { LastName: LastName, startDate: startDate, endDate: endDate, selectedPage: selectedPage }, function (data) {
+            // Remove old values (<td> and nested <tr>) from table body.
+            $("#tableBody tr").remove();
+            // Populate new data (for each result from method create a new tr and td and insert into table).
+            $.each(data, function (key, val) {
+                let tableEntry =
+                    '<tr> <td>' + val.id + '</td> <td>' + val.first_name + '</td> <td>' + val.last_name + '</td> <td>' + val.email + '</td><td>' + val.created_at + '</td></tr>';
+                $("#tableBody").append(tableEntry);
+
+            });
+        });
+    }
+
+    // The event listeners
+    $("#LastName").keyup(function () {
+        checkInputField();
+    });
+    $("#startDate, #endDate").on("change", function () {
+        checkInputField();
+    })
+    $(".PageNumber").click(function(){
+        console.log($(this).text());
+        checkInputField($(this).text());
+    });
+
+});
