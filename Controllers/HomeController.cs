65495a3e24f39f4f0fe8c10e7b20719dc73b0d93
Mendel Bak <mendelbak@gmail.com>
2018-06-17 01:05:31 -0500

    Added ability for user to specify how many results per page. Still working to pass ViewBag with CurrentPage to the View in order to dynamically generate the approriate number of page numbers depending on the results of the db query

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,9 +25,9 @@ namespace AjaxPagination.Controllers
         public IActionResult Index()
         {
             // This variable determines how many results to display per "page" and subsequently how many "pages" the view needs to display them all.
-            int UsersPerPage = 3;
-            List<User> AllUsers = _context.Users.Take(UsersPerPage).ToList();
-            ViewBag.NumOfPages = (_context.Users.Count() / 3);
+            int ResultsPerPage = 3;
+            List<User> AllUsers = _context.Users.Take(ResultsPerPage).ToList();
+            ViewBag.NumOfPages = (int)Math.Ceiling((double)_context.Users.Count() / ResultsPerPage);
             ViewBag.AllUsers = AllUsers;
             return View();
         }
@@ -37,45 +37,53 @@ namespace AjaxPagination.Controllers
         // Instead of overloaded methods, I'm going to make this single FilterUsers method have default Start and End dates.
         [HttpPost]
         [Route("FilterUsers")]
-        public JsonResult FilterUsers(string LastName, DateTime? StartDate = null, DateTime? EndDate = null, int selectedPage = 1)
+        public JsonResult FilterUsers(string LastName, DateTime? StartDate = null, DateTime? EndDate = null, int selectedPage = 1, int resultsPerPage = 3)
         {
             List<User> AllUsers;
-            int UsersPerPage = 3;
+            int ResultsPerPage = resultsPerPage;
 
             // There are waaay too many if/else checks here. There has to be a better way to do this. This feels like an immensely poor implementation.
             if (LastName == "GetAllUsers" && StartDate == null && EndDate == null)
             {
-                AllUsers = _context.Users.Skip(selectedPage * UsersPerPage - 3).Take(UsersPerPage).ToList();
+                AllUsers = _context.Users.Skip(selectedPage * ResultsPerPage - ResultsPerPage).Take(ResultsPerPage).ToList();
             }
             else if (LastName == "GetAllUsers" && StartDate != null && EndDate == null)
             {
-                AllUsers = _context.Users.Where(user => (user.created_at > (DateTime)StartDate)).ToList();
+                AllUsers = _context.Users.Where(user => (user.created_at > (DateTime)StartDate)).Skip(selectedPage * ResultsPerPage - ResultsPerPage).Take(ResultsPerPage).ToList();
             }
             else if (LastName == "GetAllUsers" && EndDate != null && StartDate == null)
             {
-                AllUsers = _context.Users.Where(user => (user.created_at < (DateTime)EndDate)).ToList();
+                AllUsers = _context.Users.Where(user => (user.created_at < (DateTime)EndDate)).Skip(selectedPage * ResultsPerPage - ResultsPerPage).Take(ResultsPerPage).ToList();
             }
             else if (LastName == "GetAllUsers" && EndDate != null && StartDate != null)
             {
-                AllUsers = _context.Users.Where(user => (user.created_at > (DateTime)StartDate) && (user.created_at < (DateTime)EndDate)).ToList();
+                AllUsers = _context.Users.Where(user => (user.created_at > (DateTime)StartDate) && (user.created_at < (DateTime)EndDate)).Skip(selectedPage * ResultsPerPage - ResultsPerPage).Take(ResultsPerPage).ToList();
             }
             else if (StartDate == null && EndDate == null)
             {
-                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName)).ToList();
+                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName)).Skip(selectedPage * ResultsPerPage - ResultsPerPage).Take(ResultsPerPage).ToList();
             }
             else if (EndDate == null)
             {
-                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName) && (user.created_at > (DateTime)StartDate)).ToList();
+                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName) && (user.created_at > (DateTime)StartDate)).Skip(selectedPage * ResultsPerPage - ResultsPerPage).Take(ResultsPerPage).ToList();
             }
             else if (StartDate == null)
             {
-                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName) && (user.created_at < (DateTime)EndDate)).ToList();
+                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName) && (user.created_at < (DateTime)EndDate)).Skip(selectedPage * ResultsPerPage - ResultsPerPage).Take(ResultsPerPage).ToList();
             }
             else
             {
-                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName) && (user.created_at > (DateTime)StartDate) && (user.created_at < (DateTime)EndDate)).ToList();
+                AllUsers = _context.Users.Where(user => (user.last_name == LastName.First().ToString().ToUpper().Trim()) || (user.last_name == LastName) && (user.created_at > (DateTime)StartDate) && (user.created_at < (DateTime)EndDate)).Skip(selectedPage * ResultsPerPage - ResultsPerPage).Take(ResultsPerPage).ToList();
             }
-            ViewBag.UsersPerPage = UsersPerPage;
+
+            // I know that I'm using ViewBag poorly here. I really should make some of these items into one single object that I pass through or some other more elegant solution.
+            // How many results to display per "page".
+            ViewBag.ResultsPerPage = ResultsPerPage;
+            // How many "pages to display.
+            ViewBag.NumOfPages = (int)Math.Ceiling((double)_context.Users.Count() / ResultsPerPage);
+            // Displays the page that the user is currently on.
+            ViewBag.selectedPage = selectedPage;
+            // User/results data.
             ViewBag.AllUsers = AllUsers;
             return Json(AllUsers);
         }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
 
     // Ensures that no null values are passed (they create a compiler error).
-    function checkInputField(selectedPage) {
+    function checkInputField(selectedPage, resultsPerPage) {
         let noNullLastName = "";
         let startDate = $("#startDate").val();
         let endDate = $("#endDate").val();
@@ -13,13 +13,13 @@ $(document).ready(function () {
         } else {
             noNullLastName = $("#LastName").val().trim();
         }
-        FilterUsers(noNullLastName, startDate, endDate, selectedPage);
+        FilterUsers(noNullLastName, startDate, endDate, selectedPage, resultsPerPage);
     }
 
     // Performs the AJAX POST call to the controller.
-    function FilterUsers(LastName, startDate, endDate, selectedPage) {
+    function FilterUsers(LastName, startDate, endDate, selectedPage, resultsPerPage) {
         // POST function call with LastName as an arguement.
-        $.post("FilterUsers/", { LastName: LastName, startDate: startDate, endDate: endDate, selectedPage: selectedPage }, function (data) {
+        $.post("FilterUsers/", { LastName: LastName, startDate: startDate, endDate: endDate, selectedPage: selectedPage, resultsPerPage: resultsPerPage }, function (data) {
             // Remove old values (<td> and nested <tr>) from table body.
             $("#tableBody tr").remove();
             // Populate new data (for each result from method create a new tr and td and insert into table).
@@ -27,21 +27,37 @@ $(document).ready(function () {
                 let tableEntry =
                     '<tr> <td>' + val.id + '</td> <td>' + val.first_name + '</td> <td>' + val.last_name + '</td> <td>' + val.email + '</td><td>' + val.created_at + '</td></tr>';
                 $("#tableBody").append(tableEntry);
-
             });
+
+            // var NumOfPages = parseInt(@ViewBag.NumOfPages);
+            // var NumOfPages = @{ViewBag.NumOfPages};
+            // var NumOfPages = @Html.Raw(Json.Encode(ViewBag.NumOfPages ));
+            // var NumOfPages = @{ViewBag.NumOfPages};
+            // console.log(NumOfPages);
+            // for(var i = 0; i < 7; i++){
+            //     $("#NumOfPages").append("<a class='PageNumber'>", (i + 1), "</a>");
+            // }
         });
     }
 
     // The event listeners
+    // Filters based on last name.
     $("#LastName").keyup(function () {
         checkInputField();
     });
+
+    // Filters based on date range
     $("#startDate, #endDate").on("change", function () {
         checkInputField();
     })
+    // Navigates to anther page of results
     $(".PageNumber").click(function(){
-        console.log($(this).text());
-        checkInputField($(this).text());
+        // The argument below is the selectePage valule.
+        checkInputField($(this).text(), $("#resultsPerPage").val());
     });
+    // Changes the amount of results displayed per page.
+    $("#resultsPerPage").on("change keyup", function () {
+        checkInputField(1, $(this).val());
+    })
 
 });
